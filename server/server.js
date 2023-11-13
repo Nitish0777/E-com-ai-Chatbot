15e0f8cfe46c814ade0685f0578578789c186f0f
Nitish Nashine <nitishnashine@gmail.com>
2023-11-13 19:57:14 +0530

    error handled done

--- server/server.js
+++ server/server.js
@@ -3,6 +3,13 @@ import dotenv from "dotenv";
 import colors from "colors";
 import connectDB from "./config/database.js";
 
+//handle uncaught exceptions
+process.on("uncaughtException", (err) => {
+  console.log(`Error: ${err.message}`.red);
+  console.log("Shutting down the server due to uncaught exception");
+  process.exit(1);
+});
+
 dotenv.config({ path: "server/.env" });
 
 // Connect to database
