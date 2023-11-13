878bfb6a208f33a7c66075d2c1f5e19cba357fc0
Nitish Nashine <nitishnashine@gmail.com>
2023-11-13 16:42:32 +0530

    done error handling

--- server/app.js
+++ server/app.js
@@ -1,5 +1,6 @@
 import express from "express";
 import productRoute from "./routes/productRoute.js";
+import errorMiddleware from "./middleware/error.js";
 
 const app = express();
 
@@ -7,5 +8,8 @@ app.use(express.json());
 
 // Import routes
 app.use("/api/v1", productRoute);
+
+//middleware for error handling
+app.use(errorMiddleware);
 
 export default app;
--- server/middleware/error.js
+++ server/middleware/error.js
@@ -1,1 +1,11 @@
 import ErrorHandler from "../utils/errorHandler";
+
+module.exports = (err, req, res, next) => {
+  err.statusCode = err.statusCode || 500;
+  err.message = err.message || "Internal Server Error";
+
+  res.status(err.statusCode).send({
+    success: false,
+    error: err,
+  });
+};
