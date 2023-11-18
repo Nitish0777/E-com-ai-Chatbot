d16669e81620c41dd619f9bf77b68181c4da3dc7
Nitish Nashine <nitishnashine@gmail.com>
2023-11-18 18:33:29 +0530

    jwt error handled

--- server/controllers/userController.js
+++ server/controllers/userController.js
@@ -3,6 +3,7 @@ import catchErrors from "../middleware/catchAsyncErrors.js";
 import User from "../models/userModel.js";
 import sendToken from "../utils/jwtToken.js";
 import { sendEmail } from "../utils/sendEmail.js";
+import crypto from "crypto";
 
 //Register a user
 export const registerUser = catchErrors(async (req, res, next) => {
