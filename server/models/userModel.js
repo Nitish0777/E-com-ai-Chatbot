e70250981406993402297641a99cb8ff389cd058
Nitish Nashine <nitishnashine@gmail.com>
2023-11-18 13:33:57 +0530

    user schema done

---
+++ server/models/userModel.js
@@ -0,0 +1,41 @@
+import mongoose from "mongoose";
+import validator from "validator";
+
+const userSchema = new mongoose.Schema({
+  name: {
+    type: String,
+    required: [true, "Please enter your name"],
+    maxLength: [30, "Your name cannot exceed 30 characters"],
+    minLength: [2, "Your name must be at least 2 characters"],
+  },
+  email: {
+    type: String,
+    required: [true, "Please enter your email"],
+    unique: true,
+    validate: [validator.isEmail, "Please enter valid email address"],
+  },
+  password: {
+    type: String,
+    required: [true, "Please enter your password"],
+    minLength: [8, "Your password must be longer than 8 characters"],
+    select: false,
+  },
+  avatar: {
+    public_id: {
+      type: String,
+      required: true,
+    },
+    url: {
+      type: String,
+      required: true,
+    },
+  },
+  role: {
+    type: String,
+    default: "user",
+  },
+  resetPasswordToken: String,
+  resetPasswordExpire: Date,
+});
+
+export default mongoose.model("User", userSchema);
