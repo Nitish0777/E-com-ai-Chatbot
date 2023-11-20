393bdded25b332c1822b01d974e121ae73d63ed1
Nitish Nashine <nitishnashine@gmail.com>
2023-11-20 22:10:13 +0530

    done footer

--- client/src/App.js
+++ client/src/App.js
@@ -1,11 +1,22 @@
+import React from "react";
 import "./App.css";
 import Header from "./components/layout/Header/Header.js";
 import { BrowserRouter as Router, Route } from "react-router-dom";
+import webfont from "webfontloader";
+import Footer from "./components/layout/Footer/Footer.js";
 
 function App() {
+  React.useEffect(() => {
+    webfont.load({
+      google: {
+        families: ["Roboto:300,400,700", "sans-serif"],
+      },
+    });
+  }, []);
   return (
     <Router>
       <Header />
+      <Footer />
     </Router>
   );
 }
