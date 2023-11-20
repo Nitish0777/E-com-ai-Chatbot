cc5e53ec52a7bc344f607a01dec5f8a5a2d71ed9
Nitish Nashine <nitishnashine@gmail.com>
2023-11-20 18:20:40 +0530

    Logo not visible

--- client/src/App.js
+++ client/src/App.js
@@ -1,10 +1,12 @@
 import "./App.css";
+import Header from "./components/layout/Header/Header.js";
+import { BrowserRouter as Router, Route } from "react-router-dom";
 
 function App() {
   return (
-    <div className="App">
-      <h1>Hello, World!</h1>
-    </div>
+    <Router>
+      <Header />
+    </Router>
   );
 }
 
